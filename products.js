c38faf69c432d473590c7f06e9035670263f326e
filipesantos1 <filipedsantos78@gmail.com>
2024-07-26 09:48:55 -0300

    Primeiro Commit - Projeto Burguer

---
+++ products.js
@@ -0,0 +1,15 @@
+const menuOptions = [
+
+    { name: 'X-Salada', price: 30, vegan: false, src: './assets/xsalada.jpeg'},
+    
+    { name: 'X-Bacon', price: 34, vegan: false, src: './assets/xbacon.jpeg' },
+    
+    { name: 'X-Bacon Egg', price: 39, vegan: false, src: './assets/xbaconegg.jpeg' },
+    
+    { name: 'Monstruoso', price: 50, vegan: false, src: './assets/monstruoso.jpeg' },
+    
+    { name: 'Big Vegano', price: 55, vegan: true, src: './assets/xvegan.jpeg' },
+    
+    { name: 'X-Vegan', price: 45, vegan: true, src: './assets/monstruosoVegan.jpeg' },
+    
+    ]
